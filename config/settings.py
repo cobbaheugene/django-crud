2418d998503aea9abfb1dcd2bbe0148f31db9930
Eugene Cobbah <cobbaheugene@gmail.com>
2025-09-10 22:59:13 +0000

    set configurations for hosting

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-gislkhwf!c8sk!$b%5$&fghi)tutbhkvih2@l!j4==82yy%&ae"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
